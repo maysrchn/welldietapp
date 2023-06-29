c3e554d6c6fc6973f1a34e80d9bbffadedf0f244
maysrchn <srchnmay489@gmail.com>
2023-06-29 11:15:10 +0700

    inputUI

--- streamlit_welldiet.py
+++ streamlit_welldiet.py
@@ -1,6 +1,27 @@
 import streamlit as st
-st.write('Hello, *World! นี่เมย์เองงงงง ง้อบแง้บ\n\nว้าวๆ \n\n\n\n วู้ววว\n\n* :sunglasses:')
+st.title('Well Diet')
+st.header('NCDs Risk Detection and Lifestyle Modification Therapy')
 
+st.selectbox('Gender',['Male','Female'])
+st.number_input('Age', 18,120)
+st.number_input('Current Weight', 30,300)
+st.number_input('Height')
+st.number_input('Waist')
+
+# st.checkbox('yes')
+
+# st.radio('Pick your gender',['Male','Female'])
+
+# st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+# st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+# st.slider('Pick a number', 0,50)
+
+# st.text_input('Email address')
+# st.date_input('Travelling date')
+
+
+
+st.button('Click')
 
 #cd /Users/may/Desktop/welldiet.py/ 
 #streamlit run streamlit_welldiet.py
