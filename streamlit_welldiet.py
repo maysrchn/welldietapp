edc97b9da30a1579e6917c214b8136bd9efa6bc4
maysrchn <srchnmay489@gmail.com>
2023-06-29 09:50:20 +0700

    testwow

--- streamlit_welldiet.py
+++ streamlit_welldiet.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.write('Hello, *World! นี่เมย์เองงงงง ง้อบแง้บ* :sunglasses:')
+st.write('Hello, *World! นี่เมย์เองงงงง ง้อบแง้บ\n\nว้าวๆ \n\n\n\n วู้ววว\n\n* :sunglasses:')
 
 
 #cd /Users/may/Desktop/welldiet.py/ 
